d5b8380e058c67584fc42745e9939d8996a075d2
thebrahminator <vishwa.nath@outlook.com>
2020-10-08 19:17:03 -0500

    Added running script

---
+++ multitier/run_trails.sh
@@ -0,0 +1,8 @@
+./lrufifosim.o $1 $2 $3 $4 1 &
+./lrufifosim.o $1 $2 $3 $4 0 &
+./lrumrusim.o $1 $2 $3 $4 1 &
+./lrumrusim.o $1 $2 $3 $4 0 &
+./lrulrusim.o $1 $2 $3 $4 1 &
+./lrulrusim.o $1 $2 $3 $4 0 &
+./lrulfusim.o $1 $2 $3 $4 1 &
+./lrulfusim.o $1 $2 $3 $4 0 &
